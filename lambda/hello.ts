884aceb43353722a4f1b46251e77a6410a3e5986
arpadt <a.arpad.toth@gmail.com>
2019-08-27 21:24:03 +1000

    Add API Gateway to the stack

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -1,9 +1,10 @@
 exports.handler = async function(event: AWSLambda.APIGatewayEvent) {
   console.log('REQUEST', JSON.stringify(event, null, 2))
 
-  return {
+  const response = {
     statusCode: 200,
-    headers: 'Content-Type: text/plain',
-    body: `Hello CDK! You've hit ${ event.path }.\n`
+    headers: { 'Content-Type': 'text/plain' },
+    body: JSON.stringify(`Hello CDK! You've hit ${ event.path }\n`),
   }
+  return response
 }
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,8 +1,19 @@
-import cdk = require('@aws-cdk/core');
+import * as cdk from '@aws-cdk/core'
+import * as lambda from '@aws-cdk/aws-lambda'
+import * as apigw from '@aws-cdk/aws-apigateway'
 
 export class CdkWorkshopStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_8_10,
+      code: lambda.Code.asset('lambda'), // the library from which lambda is called, relative from where cdk is run
+      handler: 'hello.handler',
+    })
+
+    new apigw.LambdaRestApi(this, 'Endpoint', {
+      handler: hello,
+    })
   }
 }
